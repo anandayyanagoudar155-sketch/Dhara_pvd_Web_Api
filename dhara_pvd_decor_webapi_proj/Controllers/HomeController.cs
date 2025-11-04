ad3a2278eb5a3f67b3e3e7862137d18cae6d83cd
unknown <anandayyanagoudar155@gmail.com>
2025-11-04 14:33:08 +0530

    Initial commit

---
+++ dhara_pvd_decor_webapi_proj/Controllers/HomeController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Data;
+using System.Data.SqlClient;
+using System.Xml;
+
+namespace dhara_pvd_decor_webapi_proj.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly  IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration) 
+        { 
+        
+            _configuration = configuration;
+        
+        }
+
+
+        [HttpPost("login")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Login([FromBody] UserLoginRequest request) {
+
+            var connectionstring = _configuration.GetConnectionString("DefaultConnection");
+
+            try
+            {
+
+                using (var connection = new SqlConnection(connectionstring))
+                {
+
+                    string spName = "sp_user_mast_ins_upd_del";
+
+                    var parameters = new DynamicParameters();
+                    parameters.Add("@action", "login");
+                    parameters.Add("@user_name", request.Email);
+                    parameters.Add("@user_password", request.Password);
+
+                    var user = await connection.QueryFirstOrDefaultAsync<User>(
+                    spName,
+                    parameters,
+                    commandType: CommandType.StoredProcedure
+                    );
+
+                    if (user != null)
+                    {
+                        return Ok(new
+                        {
+                            user_id = user.user_id,
+                            user_name = user.user_name,
+                            user_role = user.user_role,
+                            is_login = user.is_login
+                        });
+                    }
+                    else
+                    {
+                        return BadRequest(new { message = "Invalid Email Or Password." });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.Message });
+
+            }   
+        
+        }
+
+
+
+
+
+
+
+
+        public class UserLoginRequest
+        {
+            public string Email { get; set; } = "";
+            public string Password { get; set; } = "";
+
+        }
+
+        public class User
+        {
+            public int user_id { get; set; } = 0;
+            public string user_name { get; set; } = "";
+            public string user_role { get; set; } = "";
+            public bool is_login { get; set; } = false;
+        }
+
+    }
+}
---
+++ dhara_pvd_decor_webapi_proj/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddCors(Options => {
+        Options.AddPolicy("AllowAll", policy => {
+            policy.AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowAnyOrigin();
+        });
+    });
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseCors("AllowAll");
+
+app.Run();
